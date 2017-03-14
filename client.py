d1ca179d5acede712d437598c3c86c01aef12033
Jack <jack.wilson.east@gmail.com>
2017-03-14 12:15:48 -0700

    writing to log file alphabetically

--- client.py
+++ client.py
@@ -1,28 +1,42 @@
 import sys
+import socket
+portA = 5678
 
-#def commands():
-if(len(sys.argv) != 2):
-    print "usage: a.out port filename"
-    exit(1);
+def commands():
+    if(len(sys.argv) != 3):
+        print "usage: a.out port filename"
+        exit(1);
+#    print sys.argv[0]
+#    print sys.argv[1]
+#    print sys.argv[2]
 
-print sys.argv[0]
-print sys.argv[1]
+#def connection(portA, values):
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('127.0.0.1', int(portA)))
+
+def connection(portA, values):
+    print "Client: connected"    
+    s.send(str(values))
+    s.close
 
 
+#commands()
 try:
-	f = open(sys.argv[1]);
-	for line in f:
-		line = line.strip('\n')
-		words = line.split(' ')
-		for w in words:
-			print w
-
-	f.close()
+    values = ""
+    f = open(sys.argv[1]);
+    for line in f:
+	line = line.strip('\n')
+	words = line.split(' ')
+	for w in words:
+	    print w
+#	    values += w + " "
+	    connection(portA, w);
 
 except Exception as ex:
-	print ex
-	exit(1);
-
-
-
-print("hello world")
+   print ex
+   exit(1);
+    
+print(values)  
+#portA = sys.argv[1]
+portA = 5678
+#connection(portA, values)                   
--- server.py
+++ server.py
@@ -1,24 +1,78 @@
 import socket
 import sys
+import threading
+import time
 
+mutex = threading.Lock()
+Database = {'jack': 2000, 'aohnny': 1999 }
+
+
+def fx(arg):
+    mutex.acquire()
+    print arg, ": is running..."
+    if(arg == 5):
+	time.sleep(14)
+
+    mutex.release()
+
+threads = []
+
+#for i in range(10):
+#    t = threading.Thread(target=fx, args=(i,))
+#    t.start()
+#    threads.append(t)
+
+#for t in threads:
+#    t.join()
+
+
+print Database
+
+def printToLog():
+    f = open("log.txt", "a")
+    for key, value in sorted(Database.items()):
+	f.write('%s:%s\n' % (key, value))
+    f.close()
 
 try:
 	s = socket.socket( socket.AF_INET, socket.SOCK_STREAM)
 	s.bind(('localhost', 5678))
 	s.listen(5)
-
 	print "Server: waiting to accept()"
-#	(conn, addr) = s.accept()
+	(conn, addr) = s.accept()
 
 	print 'Server: accepted with address', addr
 
-#	data = conn.recv(80)
-	print "Server: received", data
+	data1 = conn.recv(80)
+	print "Server: received", data1
+	data2 = conn.recv(80)
+	print "Server: received", data2
+	data3 = conn.recv(80)
+	print "Server: received", data3
+	print "Server: conn?", conn
+	x = 0
+	if(data2 == "credit"):
+	    if data1 in Database.keys():
+		x = Database[data1]   
+		x += int(data3)
+		Database[data1] = x
+	    else:
+		Database[data1] = data3
+	if(data2 == "debit"):
+	    if data1 in Database.keys():
+		x = Database[data1]      
+        	x -= int(data3)
+        	Database[data1] = x
+            else:
+		Database[data1] = (int(data3)*-1)    
 
-
+	printToLog();
 	s.close()
 
 
 except Exception as err:
 	print err
 	exit(1)
+
+
+print Database
