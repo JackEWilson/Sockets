10c3c09337f322122779beedc08f1fb7c1e1dbdb
Jack <jack.wilson.east@gmail.com>
2017-03-16 16:42:20 -0700

    threading working with mutex. adding comments

--- client.py
+++ client.py
@@ -1,46 +1,35 @@
 import sys
 import socket
 import time
-portA = 5678
 
-def commands():
-    if(len(sys.argv) != 3):
-        print "usage: a.out port filename"
-        exit(1);
-#    print sys.argv[0]
-#    print sys.argv[1]
-#    print sys.argv[2]
+if(len(sys.argv) != 3):                
+    print "usage: a.out port filename"
+    exit(1);
 
-#def connection(portA, values):
+portA = sys.argv[1] #5678
+inputA = sys.argv[2]
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('127.0.0.1', int(portA)))
 
 def connection(portA, values):
-    print "Client: connected"    
     s.send(str(values))
     s.close
 
-
-#commands()
 try:
-    values = ""
-    f = open(sys.argv[1]);
+    f = open(inputA);
     for line in f:
 	line = line.strip('\n')
+	line = line.replace('$', '')
 	words = line.split(' ')
 	for w in words:
 	    print w
-#	    values += w + " "
 	    connection(portA, w);
 	    time.sleep(.1)
-	
     connection(portA, "end");
+    f.close()	
 
 except Exception as ex:
    print ex
    exit(1);
     
-print(values)  
-#portA = sys.argv[1]
-portA = 5678
-#connection(portA, values)                   
--- server.py
+++ server.py
@@ -8,28 +8,16 @@ mutex = threading.Lock()
 Database = {}
 threads = []
 
-#for i in range(10):
-#t = threading.Thread(target=fx, args=(i,))
-#t.start()
-#threads.append(t)
-
-#for t in threads:
-#t.join()
-
-#def fx(arg):
-#	mutex.acquire()
-#	print arg, ": is running..."
-#	if(arg == 5):
-#	time.sleep(14)
-#
-#	mutex.release()
+if(len(sys.argv) != 2):                
+    print "usage: a.out port"
+    exit(1)
+else:
+    portA = sys.argv[1]
 
 def catchSignal(x, f):
 	for t in threads:
 		t.join()
-	print Database
 	printToLog()
-	print "made it babe"
 	s.close()
 
 def printToLog():
@@ -38,19 +26,16 @@ def printToLog():
 		f.write('%s $%s\n' % (key, value))
 	f.close()
 
-
-def handleThreads(conn):
+def handleThreads(conn, i):
+	print "thread: ", i
 	check = 1
 	while check == 1:
 		data1 = conn.recv(80)
 		if(data1 != "end" and data1 != ""):
-			print "Server: received d1", data1
 			data2 = conn.recv(80)
-			print "Server: received d2", data2
 			data3 = conn.recv(80)
-			print "Server: received d3", data3
-			print "Server: conn? ", conn
 			x = 0
+			mutex.acquire()
 			if(data2 == "credit"):
 				if data1 in Database.keys():
 					x = int(Database[data1])   
@@ -58,13 +43,14 @@ def handleThreads(conn):
 					Database[data1] = int(x)
 				else:
 					Database[data1] = int(data3)
-			if(data2 == "debit"):
+			elif(data2 == "debit"):
 				if data1 in Database.keys():
 					x = int(Database[data1])  
 					x -= int(data3)
 					Database[data1] = x
 				else:
 					Database[data1] = (int(data3)*-1)
+			mutex.release()
 		else:
 			check = 0
 			conn.close()
@@ -72,17 +58,18 @@ def handleThreads(conn):
 
 try:
 	s = socket.socket( socket.AF_INET, socket.SOCK_STREAM)
-	s.bind(('localhost', 5678))
+	s.bind(('localhost', int(portA)))
 	s.listen(5)
-	print "Server: waiting to accept()"
+#	print "Server: waiting to accept()"
+	i = 0;
 	while True:
 		signal.signal(signal.SIGINT, catchSignal)
 		(conn, addr) = s.accept()
-		print 'Server: accepted with address', addr
-		t = threading.Thread(target=handleThreads, args=(conn,))
+#		print 'Server: accepted with address', addr
+		t = threading.Thread(target=handleThreads, args=(conn, i))
+		i += 1
 		t.start()
 		threads.append(t)
-
 
 except Exception as err:
 	print err
