b7b874971a2595c46f16ca5689a5a2b98bf5b51a
Jack <jack.wilson.east@gmail.com>
2017-03-16 14:35:57 -0700

    changing to laptop

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ import signal
 
 mutex = threading.Lock()
 Database = {}
+threads = []
 
 def catchSignal(x, f):
 	printToLog();
@@ -13,71 +14,83 @@ def catchSignal(x, f):
 	s.close()
 
 def fx(arg):
-    mutex.acquire()
-    print arg, ": is running..."
-    if(arg == 5):
-	time.sleep(14)
+	mutex.acquire()
+	print arg, ": is running..."
+	if(arg == 5):
+		time.sleep(14)
 
-    mutex.release()
-
-threads = []
-
-#for i in range(10):
-#    t = threading.Thread(target=fx, args=(i,))
-#    t.start()
-#    threads.append(t)
-
-#for t in threads:
-#    t.join()
+	mutex.release()
 
 
-print Database
 
 def printToLog():
-    f = open("log.txt", "a")
-    for key, value in sorted(Database.items()):
-	f.write('%s %s\n' % (key, value))
-    f.close()
+	f = open("log.txt", "w")
+	for key, value in sorted(Database.items()):
+		f.write('%s %s\n' % (key, value))
+	f.close()
 
-try:
-	s = socket.socket( socket.AF_INET, socket.SOCK_STREAM)
-	s.bind(('localhost', 5678))
-	s.listen(5)
-	print "Server: waiting to accept()"
-	signal.signal(signal.SIGINT, catchSignal)
-	(conn, addr) = s.accept()
-	print 'Server: accepted with address', addr
-	check = 1
-    	while check == 1 :
-		data1 = conn.recv(80)
-		if(data1 != "end"):
-			print "Server: received", data1
-			data2 = conn.recv(80)
-			print "Server: received", data2
-			data3 = conn.recv(80)
-			print "Server: received", data3
-			print "Server: conn?", conn
-			x = 0
-			if(data2 == "credit"):
-				if data1 in Database.keys():
-					x = Database[data1]   
-					x += int(data3)
-					Database[data1] = x
-				else:
-			    		Database[data1] = data3
-			if(data2 == "debit"):
-				if data1 in Database.keys():
-					x = Database[data1]      
-	    	    			x -= int(data3)
-	        			Database[data1] = x
-	        		else:
-					Database[data1] = (int(data3)*-1)    
-		else:
-	    		check = 0
+
+s = socket.socket( socket.AF_INET, socket.SOCK_STREAM)
+def begin():
+	try:
+		s.bind(('localhost', 5678))
+		s.listen(5)
+		print "Server: waiting to accept()"
+		check1 = 1
+		signal.signal(signal.SIGINT, catchSignal)
+		(conn, addr) = s.accept()
+		print 'Server: accepted with address', addr
+		check = 1
+		i = 0;
+		obj = []	
+		while check == 1 :
+			data1 = conn.recv(80)
+			if(data1 != "end"):
+				print "Server: received", data1
+				data2 = conn.recv(80)
+				print "Server: received", data2
+				data3 = conn.recv(80)
+				print "Server: received", data3
+				print "Server: conn?", conn
+				x = 0
+				i += 1
+				obj = [data1, data2, data3]
+	#			t = threading.Thread(target=dataHandler, args=(i,obj))
+	#			t.start()
+	#			threads.append(t)
+			else:
+				check = 0;
+		time.sleep(5)
+		for t in threads:
+			t.join()
+
+
+	except Exception as err: 
+		print err
+		exit(1)
+		printtoLog()
+
+def dataHandler(i, obj):
+		mutex.acquire()
+		data1 = obj[0]
+		data2 = obj[1]
+		data3 = obj[2]
+		print i, " thread is here"
+		if(data2 == "credit"):
+			if data1 in Database.keys():
+				x = int(Database[data1])   
+				x += int(data3)
+				Database[data1] = x
+			else:
+				Database[data1] = int(data3)
+		if(data2 == "debit"):
+			if data1 in Database.keys():
+				x = int(Database[data1])  
+				x -= int(data3)
+				Database[data1] = x
+			else:
+				Database[data1] = (int(data3)*-1)
+		mutex.release()
 	
-except Exception as err:
-    print err
-    exit(1)
-
-
+begin()
 print Database
