eb476ef09406269d09cf1f6f56574674d30a5beb
Jack <jack.wilson.east@gmail.com>
2017-03-17 16:28:20 -0700

    switched to send by line, also error check for float

--- client.py
+++ client.py
@@ -3,11 +3,11 @@ import socket
 import time
 
 if(len(sys.argv) != 3):                
-    print "usage: a.out port filename"
+    print "usage: python client.py port filename"
     exit(1);
 
-portA = sys.argv[1] #5678
-inputA = sys.argv[2]
+portA = sys.argv[1] #global port given in cmd line arg
+inputA = sys.argv[2] #globat input file of transactions
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('127.0.0.1', int(portA)))
@@ -16,17 +16,15 @@ def connection(portA, values):
     s.send(str(values))
     s.close
 
-try:
+try: #parses through txt file and sends information line by line
     f = open(inputA);
     for line in f:
 	line = line.strip('\n')
 	line = line.replace('$', '')
-	words = line.split(' ')
-	for w in words:
-#	    print w
-	    connection(portA, w);
-	    time.sleep(.15)
-    connection(portA, "end");
+#	print 'C: ', line
+	connection(portA, line) #sends line
+        time.sleep(.15) #had issues of it sending everything to same recv in server.
+    connection(portA, "end end end"); #when done sends a buncha 'end'
     f.close()	
 
 except Exception as ex:
--- server.py
+++ server.py
@@ -4,23 +4,23 @@ import threading
 import time
 import signal
 
-mutex = threading.Lock()
-Database = {}
-threads = []
+mutex = threading.Lock() #mutex
+Database = {} #dict
+threads = [] #keep track of threads
 
 if(len(sys.argv) != 2):                
-    print "usage: a.out port"
+    print "usage: python server.py port"
     exit(1)
 else:
-    portA = sys.argv[1]
+    portA = sys.argv[1] #Global port given as cmd line arg
 
-def catchSignal(x, f):
+def catchSignal(x, f): #catches kill signal, joins threads, prints to log.txt, and closes socket
 	for t in threads:
 		t.join()
 	printToLog()
 	s.close()
 
-def printToLog():
+def printToLog(): #prints sorted dict to log.txt
 	f = open("log.txt", "w")
 	for key, value in sorted(Database.items()):
 		f.write('%s $%s\n' % (key, value))
@@ -29,12 +29,19 @@ def printToLog():
 def handleThreads(conn):
 	check = 1
 	while check == 1:
-		data1 = conn.recv(80)
+		line = conn.recv(80) #receives information from client line by line
+		words = line.split(' ')
+		data1 = words[0]
+		data2 = words[1]
+		data3 = words[2]
+		try:
+			float(data3) #makes sure data3 is float
+		except ValueError:
+			print "found float"
+			data2 = "end"
 		if(data1 != "end" and data1 != ""):
-			data2 = conn.recv(80)
-			data3 = conn.recv(80)
 			x = 0
-			mutex.acquire()
+			mutex.acquire() #starts mutex when accessing global dict
 			if(data2 == "credit"):
 				if data1 in Database.keys():
 					x = float(Database[data1])   
@@ -51,19 +58,19 @@ def handleThreads(conn):
 					Database[data1] = (float(data3)*-1)
 			mutex.release()
 		else:
-			check = 0
+			check = 0 #ends thread
 			conn.close()
 
 try:
-	s = socket.socket( socket.AF_INET, socket.SOCK_STREAM)
+	s = socket.socket( socket.AF_INET, socket.SOCK_STREAM) #global socket
 	s.bind(('localhost', int(portA)))
 	s.listen(5)
 	while True:
 		signal.signal(signal.SIGINT, catchSignal)
-		(conn, addr) = s.accept()
+		(conn, addr) = s.accept() 
 		t = threading.Thread(target=handleThreads, args=(conn,))
 		t.start()
-		threads.append(t)
+		threads.append(t) #keeps track of threads
 
 except Exception as err:
 	print err
